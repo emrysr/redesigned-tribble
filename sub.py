1de1711591aa29f526db5d0fcd82f7b440e1a1a4
Emrys <emrys@openenergymonitor.org>
2018-10-16 21:00:27 +0100

    reformatted python

--- sub.py
+++ sub.py
@@ -1,6 +1,11 @@
-import sys, time, logging, json
-import paho.mqtt.client as paho
+import json
+import logging
+import sys
+import time
+
 import requests as requests
+
+import paho.mqtt.client as paho
 
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 
@@ -20,15 +25,16 @@ client = None
 
 logging.debug("Settings: %s, %s, %s, %s", clientId, broker, pubTopic, subTopic)
 
+
 def on_connect(client, obj, flags, rc):
     """ function called when connection is made to mqtt server 
-    
+
     Attributes:
         client -- the instance of the mqtt client that connected
         obj -- the private user data as set in Client() or user_data_set()
         flags -- response flags sent by the broker (dict) 
         rc -- response code
-    
+
     """
     global counter, retry, delay
     logging.info("Connected...")
@@ -37,7 +43,7 @@ def on_connect(client, obj, flags, rc):
     if rc == 0:
         counter = 0
         logging.debug("Subscribing to \"%s\"", subTopic)
-        client.subscribe(subTopic) #subscribe
+        client.subscribe(subTopic)  # subscribe
     else:
         if counter < retry:
             logging.debug("Reconnecting...")
@@ -50,42 +56,45 @@ def on_connect(client, obj, flags, rc):
 
 def on_message(client, obj, msg):
     """ function called when message is returend from the server 
-    
+
     Attributes:
         client -- the instance of the mqtt client that received the message
         obj -- the private user data as set in Client() or user_data_set()
         msg - an instance of MQTTMessage. This is a class with members topic, payload, qos, retain
-    
+
     """
     logging.debug(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
     call_api(msg)
     pass
 
+
 def on_publish(client, obj, mid):
     """ function called when message is returend from the server 
-    
+
     Attributes:
         client -- the instance of the mqtt client that published
         obj -- the private user data as set in Client() or user_data_set()
         mid -- message identifier
-    
+
     """
     logging.debug("Message ID: " + str(mid))
     pass
 
+
 def on_subscribe(client, obj, mid, granted_qos):
     """ function called when the broker has acknowledged the subscription 
-    
+
     Attributes:
         client -- the instance of the mqtt client that subscribed
         obj -- the private user data as set in Client() or user_data_set()
         mid -- message identifier
         granted_qos -- list of integers that give the QoS level the broker has granted
-    
+
     """
     logging.debug("Subscribed: %s %s" % str(mid), str(granted_qos))
 
-def on_disconnect(client, userdata,rc=0):
+
+def on_disconnect(client, userdata, rc=0):
     """ function called when the client disconnects from the broker
 
     Attributes
@@ -107,6 +116,7 @@ def on_disconnect(client, userdata,rc=0):
         exit()
 ###
 
+
 def call_api(msg):
     """ Relay the message payload as an API call on the local emonCMS
 
@@ -118,42 +128,45 @@ def call_api(msg):
     request = msg.payload.decode("utf-8")
     send_response(requests.get(request))
 
+
 def send_response(response):
     """ Forward the API call response (JSON) to another 
     MQTT topic the opposite client is subscibed to
-    
+
     Attributes
         response -- json payload for the mqtt message
-    
+
     """
     # print(response.json())
     logging.debug("Sending API response to: \"%s\"" % pubTopic)
-    pub_response = client.publish(pubTopic, json.dumps(response.json())) #publish
+    pub_response = client.publish(
+        pubTopic, json.dumps(response.json()))  # publish
     logging.debug("PUBLISHED: %s", paho.error_string(pub_response.rc))
     # pub_response.wait_for_publish()
 
+
 def connect():
     """ calls the mqtt client connection method 
-        
+
         counts number of connection attempts
     """
-    global counter,broker,port,username,password
+    global counter, broker, port, username, password
     logging.debug("Attempt %s" % counter)
     counter += 1
     client.username_pw_set(username, password)
-    client.connect(broker, port, 60) #connect
+    client.connect(broker, port, 60)  # connect
 
 
 def initialize():
     """ init function with exception handling.
     client.loop_forever() keeps looping this code until stop_loop() called
-    
+
     """
     global client, clientId
     try:
         logging.info("\nStart...")
 
-        client = paho.Client(clientId) 
+        client = paho.Client(clientId)
         client.on_connect = on_connect
         client.on_message = on_message
         client.on_publish = on_publish
@@ -162,17 +175,17 @@ def initialize():
 
         logging.info("Connecting to: %s " % broker)
         connect()
-        
+
         logging.debug("Started loop...")
         client.loop_forever()
 
     except TypeError:
         logging.debug('Error creating connection.')
-        return 
+        return
 
     except ValueError as err:
         logging.debug("%s: %s" % err, err.args)
-        return 
+        return
 
     except Exception as inst:
         logging.debug("Error: %s. %s", inst.args[0], inst.args[1])
@@ -181,7 +194,7 @@ def initialize():
     finally:
         client.loop_stop()
         logging.info("Exit\n")
-        return 
+        return
 
 #####
 
